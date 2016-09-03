e54805525869d9a640305a20864b7aa453ce3838
mac <mac@stefanie.local>
2016-09-03 16:12:58 +0100

    Initial Commit

---
+++ Tabbed application add work list/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed application add work list
+//
+//  Created by mac on 03/09/2016.
+//  Copyright © 2016 Rachidovitch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
